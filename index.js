39911e88a81f9843b803c254226695cca3707770
Mihlali Solwandle <mihlalisolwandle705@gmail.com>
2023-03-01 16:46:38 +0200

    new structure

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-let pageTwoNavBtn = document.getElementById('nav-page2')
-let section2 = document.getElementById('section2')
-let landingPage = document.getElementById('landingPage')
+let toggle = document.getElementById('toggle')
+let nav = document.getElementById('nav')
+let position = document.getElementById('position')
+let socialIcon = document.getElementsByClassName("socialIcon")
 
 
-
-let textHolderLeft = document.getElementById('text-holder')
-let textHolderRight = document.getElementById('text-holder2')
-
-
-pageTwoNavBtn.addEventListener("click",()=>{
-    textHolderLeft.style.display = "none"
-    textHolderRight.style.display = "none"
-    landingPage.style.display = "none"
-    section2.style.display = "flex"
-    textHolderLeft.innerText = "JAVA"
-    textHolderRight.innerText = "SCRIPT"
-
+toggle.addEventListener("click",()=>{
+    position.style.color = "black"
+    nav.style.background = "black"
+    document.body.style.background = "white"
 })
