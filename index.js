798742259537c3911eaa934d7417b1d95ebbbd69
Mihlali Solwandle <mihlalisolwandle705@gmail.com>
2023-03-02 12:17:13 +0200

    added new sections

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-let toggle = document.getElementById('toggle')
-let nav = document.getElementById('nav')
-let position = document.getElementById('position')
-let socialIcon = document.getElementsByClassName("socialIcon")
 
 
-toggle.addEventListener("click",()=>{
-    position.style.color = "black"
-    nav.style.background = "black"
-    document.body.style.background = "white"
-})
+
+
+
+
