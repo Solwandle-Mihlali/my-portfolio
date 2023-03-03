418bcc5f777a302ab90ac6163dec23ebd95c6467
Mihlali Solwandle <mihlalisolwandle705@gmail.com>
2023-03-03 13:13:46 +0200

    section2 complete for mobile

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let mobileToggleMenu = document.getElementById('mobile-toggle-menu')
 let mobileNav = document.getElementById('mobile-nav')
 let mobileClose = document.getElementById('mobile-close-nav')
+let jobTitle = document.getElementById('jobTitle')
 
+setTimeout(()=>{
+ 
+    
 
-
+})
 
 mobileToggleMenu.addEventListener("click",()=>{
    
