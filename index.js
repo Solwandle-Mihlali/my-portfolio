f8950bd9943ed26183e679b20e1af8ea6e891003
Mihlali Solwandle <mihlalisolwandle705@gmail.com>
2023-03-03 09:58:59 +0200

    added mobile nav responsiveness

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+let mobileToggleMenu = document.getElementById('mobile-toggle-menu')
+let mobileNav = document.getElementById('mobile-nav')
+let mobileClose = document.getElementById('mobile-close-nav')
 
 
+
+
+mobileToggleMenu.addEventListener("click",()=>{
+   
+    mobileNav.style.top = "0%"
+  
+})
+
+mobileClose.addEventListener("click",()=>{
+
+    mobileNav.style.top = "-100%"
+})
 
 
 
